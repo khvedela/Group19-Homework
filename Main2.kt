d2dc4f483a738573195fcdd699895e9849ee358f
David Khvedelidze <45176754+khvedela@users.noreply.github.com>
2020-10-28 23:05:23 +0400

    Add files via upload

---
+++ Main2.kt
@@ -0,0 +1,48 @@
+fun main(){
+    val f1 = Fraction(3F,4F)
+
+    val f2 = Fraction(3F,4F)
+
+
+    println(f1 == f2)
+    println(f1)
+    println(f2)
+    println(f1 + f2)
+    println(f1 * f2)
+}
+class Fraction(n :Float, d :Float) {
+
+    private var numerator: Float = n
+    private var denominator: Float = d
+
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Fraction) {
+            if (numerator * other.denominator / denominator == other.numerator) {
+                return true
+            }
+        }
+        return false
+    }
+    override fun toString(): String {
+        return "$numerator/$denominator"
+    }
+
+    operator fun plus(other : Any?): Float {
+        if (other is Fraction) {
+            return(numerator/denominator + other.numerator/other.denominator)
+        }
+        return(numerator/denominator)
+
+
+    }
+
+    operator fun times(other : Any?): Float {
+        if (other is Fraction) {
+            return(numerator/denominator * other.numerator/other.denominator)
+        }
+        return(numerator/denominator)
+
+    }
+
+}
